8d84d56ffbdbe5f9fccd4ec6be7e9c192a05daf0
dola-basafi <54464505+dola-basafi@users.noreply.github.com>
2019-09-03 10:48:48 +0700

    Add files via upload

--- kemampuan-dasar/Minggu 1/01/latihan/Basic.java
+++ kemampuan-dasar/Minggu 1/01/latihan/Basic.java
@@ -6,6 +6,7 @@ public class Basic {
     public static void main(String[] args) {
         int jumlah;
         Scanner input = new Scanner(System.in);
+        System.out.println("Masukkan jumlah looping");
         jumlah = input.nextInt();
         for (int i = 0; i < jumlah;i++) {
             System.out.println("knock");
